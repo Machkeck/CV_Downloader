c400b4c73b3bcb0944bec8626ab8a285e68338d1
Maciek Wlazlo <w.maciek@hotmail.com>
2017-07-28 02:58:41 +0200

    simplified sliding doors, tilted cv imitation

--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,98 @@
-/*import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Tilt from 'react-tilt'
 import Draggable, {DraggableCore} from 'react-draggable';
 
 require('./css/index.css')
+
+class Background extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      position: "closed"
+    };
+  }
+
+  render() {
+    return (
+      <div className="">
+        <Tilt className="Tilt" options={{
+          max: 25
+        }} style={{
+          height: 250,
+          width: 250
+        }}>
+          <div className="Tilt-inner cv_card">
+            👽 CV
+          </div>
+        </Tilt>
+      </div>
+    )
+  }
+}
+
+class Curtain extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      position: "closed"
+    };
+  }
+
+  clickHandle(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    let bodyRect = document.body.getBoundingClientRect(),
+      curtain = document.getElementById('box1'),
+      elemRect = curtain.getBoundingClientRect(),
+      offset = elemRect.right - bodyRect.right,
+      begin,
+      end;
+    console.log('Element is ' + offset + ' vertical pixels from <body>');
+    console.log(this)
+
+    if (this.state.position === "closed") {
+      begin = 0;
+      end = -(bodyRect.right - 60);
+      this.setState({position: "opened"});
+    } else {
+      begin = -(bodyRect.right - 60);
+      end = 0;
+      this.setState({position: "closed"});
+    }
+
+    console.log(begin, end)
+
+    curtain.animate([
+      // keyframes
+      {
+        transform: `translateX(${begin}px)`
+      }, {
+        transform: `translateX(${end}px)`
+      }
+    ], {
+      // timing options
+      duration: 1000,
+      easing: "ease-in-out",
+      fill: "forwards"
+
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div id="box1" className="curtain">
+
+          <div className="curtain_handle handle" onClick={this.clickHandle.bind(this)}></div>
+        </div>
+      </div>
+    )
+  }
+
+}
 
 //component
 class AppComponent extends React.Component {
@@ -11,28 +100,30 @@ class AppComponent extends React.Component {
   render() {
     return (
       <div>
-        <header className="page_header"><h2>Hello {this.props.name}</h2></header>
+        <header className="page_header">
+          <h2>Hello {this.props.name}</h2>
+        </header>
         <div className="page_content">
-        <Draggable axis="x" handle=".handle" defaultPosition={{
-          x: 0,
-          y: 0
-        }} position={null} grid={[25, 25]} onStart={this.handleStart} onDrag={this.handleDrag} onStop={this.handleStop}>
-          <div>
-            <div id="box1" className="curtain"><div className="curtain_handle handle"></div></div>
+          <div className="page_background">
+            <Background></Background>
           </div>
-        </Draggable>
+          <div className="draggable_wrapper">
+            <Curtain></Curtain>
+          </div>
         </div>
-        <footer className="page_footer"><p>Github Link</p></footer>
+        <footer className="page_footer">
+          <p>Github Link</p>
+        </footer>
 
       </div>
-    )
+    );
   }
 }
 
 ReactDOM.render(
   <AppComponent name="Maciek"/>, app)
-*/
-var React = require('react')
+
+/*var React = require('react')
   var ReactDOM = require('react-dom')
     import Draggable, {DraggableCore} from 'react-draggable';
     var counter = 0;
@@ -64,7 +155,7 @@ var React = require('react')
             offset = elemRect.right - bodyRect.right;
           console.log('Element is ' + offset + ' vertical pixels from <body>');
 
-          if (offset <= -400 && offset >= -500) {
+          if (offset <= -400 && offset >= -410 && counter!=1) {
             console.log("leser", ui)
 
             counter = 1;
@@ -183,4 +274,4 @@ var React = require('react')
       });
 
       ReactDOM.render(
-        <App/>, document.getElementById('app'));
+        <App/>, document.getElementById('app'));*/
